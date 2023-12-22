6535f4da393fe646c929a97f299b26fb7df713e9
Jonathan Leung <jleung@vmware.com>
2023-12-22 12:20:53 +0000

    Updated HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,8 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		System.out.println("Greetings from Spring Boot + Tanzu!!");
+		return "Hello Spring Boot + Tanzu!!";
 	}
 
 }
